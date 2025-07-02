f2e19f829d5c91e84d819a89f4ce4a85008c7a5c
island-ld777 <ldiazh777@gmail.com>
2025-07-02 17:05:14 -0400

    Implemented PostgeSQL functionalities

--- index.js
+++ index.js
@@ -1,28 +1,63 @@
 const express = require("express");
 const fs = require("fs");
 const { parse } = require("path");
+const Pool = require('pg').Pool
+require("dotenv").config();
+
+//ENVIRONMENT VARIABLES CHECK
+// console.log(process.env.DB_HOST);
+// console.log(process.env.DB_PORT);
+// console.log(process.env.DB_USERNAME);
+// console.log(process.env.DB_PASSWORD);
+
+//DB Set Up
+const pool = new Pool({
+    user: process.env.DB_USERNAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+});
+
+(async () => {
+    try {
+        await pool.query('SELECT 1');
+        console.log("Connected to Database: %s", process.env.DB_NAME);
+    }
+    catch (err) {
+        console.error("Unable to connect to DB Server: ", err);
+        process.exit(1);
+    }
+})();
+
 const filePath = "E:\\CODE\\TODO_API\\data\\data.json";
 const app = express();
-let data = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
-let data_list_length = data["todos"].length;
+//let data = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+//let data_list_length = data["todos"].length;
 
 const PORT  = 3001;
 app.use(express.json());
 
 app.get("/todos", (req, res) => {
-    res.status(200);
-    return res.json(data);
+
+    pool.query('SELECT * FROM todos', (err, results) => {
+        if (err){
+            throw err;
+        }
+        res.status(200);
+        return res.json(results.rows);
+
+    })
+
 })
 
 app.post("/todos", (req, res) => {
-    var newTask = {
-        "id": ++data_list_length,
-        "task": req.body["task"],
-        "done": false
-    }
-    data["todos"].push(newTask);
-    fs.writeFileSync(filePath, JSON.stringify(data), ['w', 'utf-8']);
-    return res.status(200).send("New Task added.");
+    pool.query('INSERT INTO todos (task, done) VALUES ($1, FALSE) RETURNING *', [req.body["task"]], (err, results) => {
+        if (err) {
+            throw err;
+        }
+        return res.status(201).json(results.rows[0]);
+    })
 })
 
 app.put("/todos/:id", (req, res) => {
@@ -30,18 +65,22 @@ app.put("/todos/:id", (req, res) => {
         body,
          params: {id},
     } = req;
-    //console.log(body);
-    //console.log(id);
     const parsedID = parseInt(id);
     if(isNaN(parsedID)) return res.status(400).send("Invalid ID");
+    pool.query(
+        'UPDATE todos SET task = $1, done = $2 WHERE id = $3 RETURNING *',
+        [body["task"], body["done"], parsedID],
+        (err, results) => {
+            if(err) {
+                throw error
+            }
 
-    const findTaskIndex = data["todos"].findIndex((task) => task.id === parsedID);
-    if(findTaskIndex === -1) return res.status(404).send("Task not found.");
-    //console.log(findTaskIndex);
-    //console.log(data["todos"][findTaskIndex]);
-    data["todos"][findTaskIndex] = {id: parsedID, ...body};
-    fs.writeFileSync(filePath, JSON.stringify(data), ['w', 'utf-8']);
-    return res.status(200).send();
+            if(results.rowCount === 0){
+                return res.status(404).send("Record not found.");
+            }
+            return res.status(202).json(results.rows[0]);
+        }
+    )
 })
 
 app.delete("/todos/:id", (req, res) => {
@@ -49,19 +88,19 @@ app.delete("/todos/:id", (req, res) => {
         body,
          params: {id},
     } = req;
-    //console.log(body);
-    //console.log(id);
     const parsedID = parseInt(id);
     if(isNaN(parsedID)) return res.status(400).send("Invalid ID");
+    pool.query('DELETE FROM todos WHERE id = $1', [parsedID], (err, results) =>{
+        if (err) {
+            throw err;
+        }
 
-    const findTaskIndex = data["todos"].findIndex((task) => task.id === parsedID);
-    if(findTaskIndex === -1) return res.status(404).send("Task not found.");
-    //console.log(findTaskIndex);
-    //console.log(data["todos"][findTaskIndex]);
-    data["todos"].splice(findTaskIndex, 1);
-    data_list_length--;
-    fs.writeFileSync(filePath, JSON.stringify(data), ['w', 'utf-8']);
-    return res.status(200).send();
+        if(results.rowCount === 0) {
+            return res.status(404).send("Record not found.");
+        }
+
+        return res.status(202).json(results.rows[0]);
+    });
 })
 
 
